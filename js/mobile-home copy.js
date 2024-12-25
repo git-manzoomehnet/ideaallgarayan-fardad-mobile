0a8fb63eab57ed2235bb87b9b4f46398af65b6c5
Zeinab <zeinabansarian6280@gmail.com>
2024-12-25 17:40:48 +0330

    qc changes

--- js/mobile-home copy.js
+++ js/mobile-home copy.js
@@ -1,6 +1,6 @@
 let mainSection = document.querySelectorAll(".mainSection"),
   paging = document.querySelector(".paging");
-mainSection.forEach((e, t) => {
+mainSection.forEach((e, a) => {
   let l = document.createElement("span");
   l.classList.add("pageSpan"), paging.appendChild(l);
 });
@@ -10,7 +10,7 @@ function raf(e) {
 }
 requestAnimationFrame(raf);
 let pageSpanAll = document.querySelectorAll(".pageSpan");
-mainSection.forEach((e, t) => {
+mainSection.forEach((e, a) => {
   gsap.to(e, {
     scrollTrigger: {
       trigger: e,
@@ -20,28 +20,28 @@ mainSection.forEach((e, t) => {
         pageSpanAll.forEach((e) => {
           e.classList.remove("activePageSpan");
         }),
-          pageSpanAll[t].classList.add("activePageSpan");
+          pageSpanAll[a].classList.add("activePageSpan");
       },
       onEnterBack: () => {
         pageSpanAll.forEach((e) => {
           e.classList.remove("activePageSpan");
         }),
-          pageSpanAll[t].classList.add("activePageSpan");
+          pageSpanAll[a].classList.add("activePageSpan");
       },
       onLeaveBack: () => {
         pageSpanAll.forEach((e) => {
           e.classList.remove("activePageSpan");
         }),
-          pageSpanAll[t].classList.add("activePageSpan");
+          pageSpanAll[a].classList.add("activePageSpan");
       },
       onLeave: () => {},
     },
   });
 }),
   gsap.registerPlugin(ScrollTrigger),
-  pageSpanAll.forEach((e, t) => {
+  pageSpanAll.forEach((e, a) => {
     e.addEventListener("click", function (e) {
-      lenis.scrollTo(`.section${t + 1}`, {});
+      lenis.scrollTo(`.section${a + 1}`, {});
     });
   });
 let defScrollTop = document.querySelector(".defScrollTop");
@@ -92,47 +92,14 @@ window.pageYOffset > 30 && (footerScrollTop.style.transform = "scale(1)"),
       },
     },
   });
-let menuBarHome = document.querySelector(".menuBar");
-menuBarHome.addEventListener("click", function (e) {
-  lenis.stop();
-});
-let closeMegaMenuHome = document.querySelector(".closeMegaMenu");
-closeMegaMenuHome.addEventListener("click", function (e) {
-  lenis.start();
-});
 
-let blogBox = document.querySelectorAll(".blogBox");
 
-blogBox.forEach(function (box, index) {
-  let newIn = index+1
-  if (newIn % 2 === 0) {
-    gsap.from(box, {
-      y: "100%",
-      duration:1,
-      opacity: 0,
-      ease: "circ.out",
-      scrollTrigger: {
-        trigger: box,
-        start: "top 80%",
-        end: "bottom bottom",
-        // markers:true,
-      },
-    });
-    //even elements are here, you can access it by box
-  } else {
-    //odd elements are here, you can access it by box
-    
-    gsap.from(box, {
-      y: "-100%",
-      duration:1,
-      opacity: 0,
-      ease: "circ.out",
-      scrollTrigger: {
-        trigger: box,
-        start: "top 80%",
-        end: "bottom bottom",
-        // markers:true,
-      },
-    });
-  }
-});
+  
+let menuBarHome = document.querySelector(".menuBar")
+menuBarHome.addEventListener("click",function (params) {
+  lenis.stop()
+})
+let closeMegaMenuHome = document.querySelector(".closeMegaMenu")
+closeMegaMenuHome.addEventListener("click",function (params) {
+  lenis.start()
+})
