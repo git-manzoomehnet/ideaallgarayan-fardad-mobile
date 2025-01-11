fd2837d8cee997485ae042be03246921ac1ed95d
Zeinab <zeinabansarian6280@gmail.com>
2025-01-11 10:58:04 +0330

    changes for checklist

--- js/footer-scrolltop.js
+++ js/footer-scrolltop.js
@@ -1,13 +1,16 @@
 
 let footerScrollTop = document.querySelector(".footerScrollTop")
-footerScrollTop.onclick = () => window.scrollTo({ top: 0, behavior: "smooth" });
-if (window.pageYOffset > 30) {
-    footerScrollTop.style.transform = "scale(1)";
-}
-window.addEventListener("scroll", function (params) {
-    if (this.window.pageYOffset > 30) {
+if (footerScrollTop) {
+  
+  footerScrollTop.onclick = () => window.scrollTo({ top: 0, behavior: "smooth" });
+  if (window.pageYOffset > 30) {
       footerScrollTop.style.transform = "scale(1)";
-    } else {
-      footerScrollTop.style.transform = "scale(0)";
-    }
-  });
+  }
+  window.addEventListener("scroll", function (params) {
+      if (this.window.pageYOffset > 30) {
+        footerScrollTop.style.transform = "scale(1)";
+      } else {
+        footerScrollTop.style.transform = "scale(0)";
+      }
+    });
+}
--- js/mobile-home.js
+++ js/mobile-home.js
@@ -1,138 +1,110 @@
 let mainSection = document.querySelectorAll(".mainSection"),
-  paging = document.querySelector(".paging");
-mainSection.forEach((e, t) => {
-  let l = document.createElement("span");
-  l.classList.add("pageSpan"), paging.appendChild(l);
-});
-const lenis = new Lenis();
+    paging = document.querySelector(".paging");
+mainSection.forEach(((e, o) => {
+    let t = document.createElement("span");
+    t.classList.add("pageSpan"), paging.appendChild(t)
+}));
+const lenis = new Lenis;
+
 function raf(e) {
-  lenis.raf(e), requestAnimationFrame(raf);
+    lenis.raf(e), requestAnimationFrame(raf)
 }
 requestAnimationFrame(raf);
 let pageSpanAll = document.querySelectorAll(".pageSpan");
-mainSection.forEach((e, t) => {
-  gsap.to(e, {
-    scrollTrigger: {
-      trigger: e,
-      start: "top 60%",
-      end: "bottom 60%",
-      onEnter: () => {
-        pageSpanAll.forEach((e) => {
-          e.classList.remove("activePageSpan");
-        }),
-          pageSpanAll[t].classList.add("activePageSpan");
-      },
-      onEnterBack: () => {
-        pageSpanAll.forEach((e) => {
-          e.classList.remove("activePageSpan");
-        }),
-          pageSpanAll[t].classList.add("activePageSpan");
-      },
-      onLeaveBack: () => {
-        pageSpanAll.forEach((e) => {
-          e.classList.remove("activePageSpan");
-        }),
-          pageSpanAll[t].classList.add("activePageSpan");
-      },
-      onLeave: () => {},
-    },
-  });
-}),
-  gsap.registerPlugin(ScrollTrigger),
-  pageSpanAll.forEach((e, t) => {
-    e.addEventListener("click", function (e) {
-      lenis.scrollTo(`.section${t + 1}`, {});
-    });
-  });
+mainSection.forEach(((e, o) => {
+    gsap.to(e, {
+        scrollTrigger: {
+            trigger: e,
+            start: "top 60%",
+            end: "bottom 60%",
+            onEnter: () => {
+                pageSpanAll.forEach((e => {
+                    e.classList.remove("activePageSpan")
+                })), pageSpanAll[o].classList.add("activePageSpan")
+            },
+            onEnterBack: () => {
+                pageSpanAll.forEach((e => {
+                    e.classList.remove("activePageSpan")
+                })), pageSpanAll[o].classList.add("activePageSpan")
+            },
+            onLeaveBack: () => {
+                pageSpanAll.forEach((e => {
+                    e.classList.remove("activePageSpan")
+                })), pageSpanAll[o].classList.add("activePageSpan")
+            },
+            onLeave: () => { }
+        }
+    })
+})), gsap.registerPlugin(ScrollTrigger), pageSpanAll.forEach(((e, o) => {
+    e.addEventListener("click", (function (e) {
+        lenis.scrollTo(`.section${o + 1}`, {})
+    }))
+}));
 let defScrollTop = document.querySelector(".defScrollTop");
-window.pageYOffset > 30 && (footerScrollTop.style.transform = "scale(1)"),
-  window.addEventListener("scroll", function (e) {
-    this.window.pageYOffset > 100
-      ? (defScrollTop.style.transform = "scale(1)")
-      : (defScrollTop.style.transform = "scale(0)");
-  }),
-  gsap.to(".whiteSection", {
+window.pageYOffset > 30 && (footerScrollTop.style.transform = "scale(1)"), window.addEventListener("scroll", (function (e) {
+    this.window.pageYOffset > 100 ? defScrollTop.style.transform = "scale(1)" : defScrollTop.style.transform = "scale(0)"
+})), gsap.to(".whiteSection", {
     scrollTrigger: {
-      trigger: ".whiteSection",
-      start: "top 80%",
-      end: "bottom 80%",
-      onEnter: () => {
-        defScrollTop.classList.add("!bg-white"),
-          (defScrollTop.querySelector("path").style.fill = "#434343");
-      },
-      onEnterBack: () => {
-        defScrollTop.classList.add("!bg-white"),
-          (defScrollTop.querySelector("path").style.fill = "#434343");
-      },
-      onLeaveBack: () => {
-        defScrollTop.classList.remove("!bg-white"),
-          (defScrollTop.querySelector("path").style.fill = "white"),
-          defScrollTop.classList.remove("!bg-medium_gray");
-      },
-      onLeave: () => {
-        defScrollTop.classList.remove("!bg-white"),
-          defScrollTop.classList.add("!bg-medium_gray"),
-          (defScrollTop.querySelector("path").style.fill = "white");
-      },
-    },
-  }),
-  defScrollTop.addEventListener("click", function (e) {
-    lenis.scrollTo(0, {});
-  }),
-  gsap.to("footer", {
+        trigger: ".whiteSection",
+        start: "top 80%",
+        end: "bottom 80%",
+        onEnter: () => {
+            defScrollTop.classList.add("!bg-white"), defScrollTop.querySelector("path").style.fill = "#434343"
+        },
+        onEnterBack: () => {
+            defScrollTop.classList.add("!bg-white"), defScrollTop.querySelector("path").style.fill = "#434343"
+        },
+        onLeaveBack: () => {
+            defScrollTop.classList.remove("!bg-white"), defScrollTop.querySelector("path").style.fill = "white", defScrollTop.classList.remove("!bg-medium_gray")
+        },
+        onLeave: () => {
+            defScrollTop.classList.remove("!bg-white"), defScrollTop.classList.add("!bg-medium_gray"), defScrollTop.querySelector("path").style.fill = "white"
+        }
+    }
+}), defScrollTop.addEventListener("click", (function (e) {
+    lenis.scrollTo(0, {})
+})), gsap.to("footer", {
     scrollTrigger: {
-      trigger: "footer",
-      start: "top 70%",
-      end: "bottom 70%",
-      onEnter: () => {
-        paging.style.opacity = "0";
-      },
-      onLeaveBack: () => {
-        paging.style.opacity = "1";
-      },
-    },
-  });
+        trigger: "footer",
+        start: "top 70%",
+        end: "bottom 70%",
+        onEnter: () => {
+            paging.style.opacity = "0"
+        },
+        onLeaveBack: () => {
+            paging.style.opacity = "1"
+        }
+    }
+});
 let menuBarHome = document.querySelector(".menuBar");
-menuBarHome.addEventListener("click", function (e) {
-  lenis.stop();
-});
+menuBarHome.addEventListener("click", (function (e) {
+    lenis.stop(), document.querySelector("body").style.overflow = "hidden"
+}));
 let closeMegaMenuHome = document.querySelector(".closeMegaMenu");
-closeMegaMenuHome.addEventListener("click", function (e) {
-  lenis.start();
-});
-
+closeMegaMenuHome.addEventListener("click", (function (e) {
+    lenis.start(), document.querySelector("body").style.overflow = "auto"
+}));
 let blogBox = document.querySelectorAll(".blogBox");
-
-blogBox.forEach(function (box, index) {
-  let newIn = index+1
-  if (newIn % 2 === 0) {
-    gsap.from(box, {
-      y: "100%",
-      duration:1,
-      opacity: 0,
-      ease: "circ.out",
-      scrollTrigger: {
-        trigger: box,
-        start: "top 80%",
-        end: "bottom bottom",
-        // markers:true,
-      },
-    });
-    //even elements are here, you can access it by box
-  } else {
-    //odd elements are here, you can access it by box
-    
-    gsap.from(box, {
-      y: "-100%",
-      duration:1,
-      opacity: 0,
-      ease: "circ.out",
-      scrollTrigger: {
-        trigger: box,
-        start: "top 80%",
-        end: "bottom bottom",
-        // markers:true,
-      },
-    });
-  }
-});
+blogBox.forEach((function (e, o) {
+    (o + 1) % 2 == 0 ? gsap.from(e, {
+        y: "50%",
+        duration: 1,
+        opacity: 0,
+        ease: "circ.out",
+        scrollTrigger: {
+            trigger: e,
+            start: "top 100%",
+            end: "bottom bottom"
+        }
+    }) : gsap.from(e, {
+        y: "50%",
+        duration: 1,
+        opacity: 0,
+        ease: "circ.out",
+        scrollTrigger: {
+            trigger: e,
+            start: "top 100%",
+            end: "bottom bottom"
+        }
+    })
+}));
